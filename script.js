43139a5352d6ed0b55a6983dd4e1d652e1707a6b
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-06-16 15:29:32 +0200

    Added change player functionality

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -25,8 +28,14 @@ btnRoll.addEventListener('click', function () {
   // 3. Check if 1, if true - swith to next player
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore; // Change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore; // Change later
   } else {
     // Switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
